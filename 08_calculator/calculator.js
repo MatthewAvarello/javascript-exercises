e73cbf478280e7cebba3e59c78679aef700808c1
Matthew Avarello <182387652+MatthewAvarello@users.noreply.github.com>
2025-01-27 14:09:08 -0500

    Completed calculator exercise.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,33 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2 
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total,currentitem) => total + currentitem, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total,currentitem) => total * currentitem, 1)
 };
 
-const power = function() {
-	
+const power = function(base,exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number == 0){
+    return 1
+  }
+  let total = number
+  for(let i = number - 1; i > 0; i--) {
+    total = total * i
+  }
+  return total
 };
-
 // Do not edit below this line
 module.exports = {
   add,
