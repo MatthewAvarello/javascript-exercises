5dbd58741b4cc900be721a5201d7714d35e9f8e2
Matthew Avarello <182387652+MatthewAvarello@users.noreply.github.com>
2025-01-28 17:09:37 -0500

    Done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(placement) {
+    if (placement < 0){
+        return "OOPS"
+    }
+    if (placement == 0) {
+        return 0
+    }
+    if (placement == 1) {
+        return 1
+    }
+    let n1 = 0, n2 = 1, Nextterm
+    placement = Number(placement)
+    for (let i = 2; placement >= i; i++) {
+        Nextterm = n1 + n2
+        n1 = n2
+        n2 = Nextterm
+    }
+    return Nextterm
 };
-
+console.log(fibonacci(6))
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,45 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const currentyear = new Date().getFullYear()
+    let orderbyage = arr.sort((per1,per2) => {
+        if (!per1.yearOfDeath){
+            per1.yearOfDeath = currentyear
+        } else if (!per2.yearOfDeath) {
+            per2.yearOfDeath = currentyear
+        }
 
+        let per1age = per1.yearOfDeath - per1.yearOfBirth
+        let per2age = per2.yearOfDeath - per2.yearOfBirth
+        return per1age - per2age
+    })
+    let oldestinarray = orderbyage.pop();
+    return oldestinarray
 };
+
+
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+
+
+  console.log(findTheOldest(people))
+  console.log(people)
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
